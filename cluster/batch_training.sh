5e36e3034f00d038694c56687448afd94aec444a
shunnnli <shunli@g.harvard.edu>
2024-03-11 19:39:00 -0400

    ~/

--- cluster/batch_training.sh
+++ cluster/batch_training.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=250G                # total memory per node (16 GB per cpu-core is default)
 #SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
-#SBATCH -o /jobs/EP-LHb-model/%j.out # STDOUT
-#SBATCH -e /jobs/EP-LHb-model/%j.err # STDERR
+#SBATCH -o ~/jobs/EP-LHb-model/%j.out # STDOUT
+#SBATCH -e ~/jobs/EP-LHb-model/%j.err # STDERR
 #SBATCH --mail-type END,FAIL
 #SBATCH --mail-user shun.li@g.harvard.edu
 
