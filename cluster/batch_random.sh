c0297d7d1ebe2df11b97cdd40b387d5f9767b4c5
shunnnli <shunli@g.harvard.edu>
2024-03-13 15:08:20 -0400

    batch

--- cluster/batch_random.sh
+++ cluster/batch_random.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p kempner_requeue # partition (queue)
+#SBATCH -p kempner # partition (queue)
 #SBATCH --account=kempner_bsabatini_lab
 #SBATCH --job-name=eplhb-models     # create a short name for your job
 #SBATCH --nodes=1                # node count
@@ -7,10 +7,10 @@
 #SBATCH --gres=gpu:1             # number of allocated gpus per node
 #SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=250G                # total memory per node (16 GB per cpu-core is default)
-#SBATCH --time=02:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=2-02:00:00          # total run time limit (HH:MM:SS)
 #SBATCH -o %j.out # STDOUT
 #SBATCH -e %j.err # STDERR
-#SBATCH --mail-type END,FAIL
+#SBATCH --mail-type START, END, FAIL
 #SBATCH --mail-user shunli@g.harvard.edu
 
 module load python/3.10.12-fasrc01
